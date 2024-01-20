c65f6f118cb241ff79cf4203f20cee2c1c0ab40a
Taylor <83951714+dallimtayl@users.noreply.github.com>
2024-01-20 21:21:53 +1300

    orb

--- entities/gun.gd
+++ entities/gun.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
+@onready var anim = $AnimationPlayer
 
-func _physics_process(delta):
+
+
+func _physics_process(_delta):
 	var enemies_in_range = get_overlapping_bodies()
 	if enemies_in_range.size() > 0:
 		var target_enemy = enemies_in_range.front()
 		look_at(target_enemy.global_position)
-		
