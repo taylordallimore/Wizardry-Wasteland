af0f9a52f86bb2dd83807d424aa1af07d5f9feb2
Sebastian Reed <reedseba@ecs.vuw.ac.nz>
2024-01-21 10:40:08 +1300

    Implementing healthbaer

---
+++ common/healthbar/healthbar.gd
@@ -0,0 +1,19 @@
+extends Control
+
+@onready var health_bar = $Healthbar
+@onready var player = get_node("/root/Level_1/player")
+
+signal health_changed(health)
+
+
+func _ready():
+
+	print(player.connect("health_updated", set_value))
+	player.connect("health_updated", set_value)
+
+
+func set_value(health):
+	health_bar.value = health 
+
+func set_max_value(max_health):
+	health_bar.max_value = max_health
