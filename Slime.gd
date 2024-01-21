5282a5f2f2d050cb1ba31bf33c92a0dfa526822f
Taylor <83951714+dallimtayl@users.noreply.github.com>
2024-01-21 15:28:04 +1300

    hello

---
+++ Slime.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+var health = 1
+
+@onready var anim = $AnimationPlayer
+@onready var player = get_node("/root/Level_1/player")
+
+func _physics_process(delta):
+	anim.play("idle")
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * 50.0
+	move_and_slide()
+
+func take_damage():
+	health -= 1
+	if health <= 0:
+		queue_free()
